642166ed98ebe286f42940855834f5da65a4d72c
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-08-23 20:08:20 +0430

    fea/ add bottom navigation

--- app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.musicplayer.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.musicplayer.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController: NavController =
+            Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.activity_main_bottom_navigation_view)
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
